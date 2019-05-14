50cb10c588d80cb3133d5aa37363f0bec255345d
Christian Brückner <c.brueckner85@gmx.net>
2019-05-14 06:50:21 +0200

    Added API file

--- Microservice.Management/Program.cs
+++ Microservice.Management/Program.cs
@@ -8,9 +8,18 @@ namespace Microservice.Management
     {
         static void Main(string[] args)
         {
+            var i = 0;
+
+            NLog.LogManager.Configuration = new NLog.Config
+                .XmlLoggingConfiguration(System.Reflection.Assembly.GetExecutingAssembly().Location + ".config");
+            var log = NLog.LogManager.GetLogger("TestLogger", typeof(Program));
+
+            var config = NLog.LogManager.Configuration;
+
+            log.Debug("Application started.");
 
             // Setup service container
-            var customerService = new CustomerService();
+            //var customerService = new CustomerService();
             
 
             // Configure all services and run in extra class/interface.
